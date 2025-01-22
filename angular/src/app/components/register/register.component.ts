aaa603985eaa569a04135c4068fc3a7be0f58811
Aashish <aashish.mahato@islingtoncollege.edu.np>
2025-01-22 16:55:46 +0545

    workin on registration form

--- angular/src/app/components/register/register.component.ts
+++ angular/src/app/components/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {MatSelectModule} from "@angular/material/select";
+import {MatDatepickerModule} from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    MatSelectModule,
+    MatDatepickerModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  protected presentDate = new Date();
 
 }
