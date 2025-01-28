f113813dff76ac8bd9aa93b07f2c292c8d56101e
aashish300 <aashishmahato300@gmail.com>
2025-01-29 00:05:43 +0545

    java setup

---
+++ java/src/main/java/artist_management_system/java/Security/Auth/CustomAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package artist_management_system.java.Security.Auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(final HttpServletRequest request, final HttpServletResponse response, final AccessDeniedException accessDeniedException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access is Denied.");
+    }
+}
---
+++ java/src/main/java/artist_management_system/java/Utils/Enum/Gender.java
@@ -0,0 +1,4 @@
+package artist_management_system.java.Utils.Enum;
+
+public class Gender {
+}
